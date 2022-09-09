3187a588419b2e5d9891a08f74175a6f7e9e44e9
aayush_anand <aayush_anand@persistent.com>
2022-09-09 19:53:18 +0530

    Made modal and that generated cards and is responsive

--- src/Shravani/App.js
+++ src/Shravani/App.js
@@ -24,7 +24,11 @@
 // export default App;
 
 import React, { useState } from "react";
-import { Modal, Button } from "react-bootstrap";
+import { Modal, Button, Card } from "react-bootstrap";
+import CreateCard from "../Cards/CardUI";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   const [showModal, setShow] = useState(false);
@@ -32,16 +36,23 @@ function App() {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  const [{items}, setItems] = useState({ items: [] });
+  const addItem = () => {
+    items.push(
+          <Col key={items.length}>
+            <CreateCard />
+          </Col>
+      );
+    setItems({ items: [...items] });
+  }
   return (
     <>
-      <div
-        className="d-flex align-items-center justify-content-center"
-        style={{ height: "100vh" }}
-      >
+      <div>
         <Button variant="primary" onClick={handleShow}>
           + Add Project
         </Button>
       </div>
+
       <Modal show={showModal} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Modal heading</Modal.Title>
@@ -51,11 +62,17 @@ function App() {
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
+          <Button variant="primary" onClick={addItem}>
+            Click Me
           </Button>
         </Modal.Footer>
       </Modal>
+
+      <React.Fragment>
+        <Row>
+          {items}
+        </Row>
+      </React.Fragment>
     </>
   );
 }
